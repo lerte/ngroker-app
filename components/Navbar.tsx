00acbd1d46ad633058f9a33b7e6033e25fcfc707
lerte smith <lerte@qq.com>
2024-04-02 21:44:00 +0800

    update docs

--- components/Navbar.tsx
+++
@@ -1,6 +0,0 @@
-const Navbar = (props) => {
-  console.log(props);
-  return <div>todo</div>;
-};
-
-export default Navbar;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "./components/Head";
 import Logo from "./components/Logo";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import EditOnGithub from "./components/EditOnGithub";
 import { DocsThemeConfig } from "nextra-theme-docs";
@@ -9,15 +8,12 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   useNextSeoProps: () => ({ titleTemplate: "%s – Ngroker" }),
   head: Head,
-  // navbar: {
-  //   component: Navbar,
-  // },
   logo: Logo,
   feedback: {
     content: null,
   },
   nextThemes: {
-    defaultTheme: "dark",
+    defaultTheme: "system",
   },
   editLink: {
     component: EditOnGithub,
